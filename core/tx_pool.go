34707ab3241058118f19051cf472605ce9a51a27
kinsuft173 <hukun@oudmon.com>
2019-01-04 16:27:04 +0800

    修改gas以及上线

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -148,10 +148,10 @@ var DefaultTxPoolConfig = TxPoolConfig{
 	PriceLimit: 1,
 	PriceBump:  10,
 
-	AccountSlots: 16,
-	GlobalSlots:  4096,
-	AccountQueue: 64,
-	GlobalQueue:  1024,
+	AccountSlots: 16000,
+	GlobalSlots:  4096000,
+	AccountQueue: 64000,
+	GlobalQueue:  1024000,
 
 	Lifetime: 3 * time.Hour,
 }
